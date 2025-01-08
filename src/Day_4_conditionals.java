7d1a696c691a9b0731ae8a4fa5b3fc37669e3576
manideepperuka12 <manideepperuka@gmail.com>
2025-01-08 21:39:34 +0530

    files added

--- src/Day_4_conditionals.java
+++ src/Day_4_conditionals.java
@@ -1,7 +1,7 @@
 public class Day_4_conditionals {
     //1.When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
-public static void main(String[] args) {
-    boolean cigarParty(int cigars, boolean isWeekend) {
+
+    public boolean cigarParty(int cigars, boolean isWeekend) {
         if(isWeekend){
           if(cigars>=40){
             return true; 
@@ -16,5 +16,4 @@ public static void main(String[] args) {
             }
           }
           }
-        }      
-}
+        }
---
+++ src/Day_5_Loops.java
@@ -0,0 +1,8 @@
+public class Day_5_Loops {
+    public static void main(String[] args) {
+        String c = "mani";
+        System.out.println(c.length());
+        System.out.println(c.charAt(0));
+        System.out.println(c.substring(2));
+    }
+}
